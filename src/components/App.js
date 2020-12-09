9088230455157c93c7977f462a57f8e46706e683
raj <rajabhi2801@gmail.com>
2020-12-09 13:51:17 +0530

    working fine

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,51 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = ({ slides }) => {
+  const [index, setIndex] = useState(0);
+  const handleRestart = () => {
+    setIndex(0);
+  };
+  const handlePrevClick = () => {
+    if (index === 0) {
+      return;
+    }
+    setIndex(index - 1);
+  };
+  const handleNextClick = () => {
+    if (index === slides.length - 1) {
+      return;
+    }
+    setIndex(index + 1);
+  };
+
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-testid="title">{slides[index].title}</h1>
+      <p data-testid="text">{slides[index].text}</p>
+      <button
+        data-testid="button-restart"
+        disabled={index === 0 ? true : false}
+        onClick={handleRestart}
+      >
+        Restart
+      </button>
+      <button
+        data-testid="button-prev"
+        disabled={index === 0 ? true : false}
+        onClick={handlePrevClick}
+      >
+        Prev
+      </button>
+      <button
+        data-testid="button-next"
+        disabled={index !== slides.length - 1 ? false : true}
+        onClick={handleNextClick}
+      >
+        Next
+      </button>
+    </>
+  );
+};
 
 export default App;
